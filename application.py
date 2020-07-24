c8947a0eafc5163882f548c10b54e624fc490749
noamazou15 <61350757+noamazou15@users.noreply.github.com>
2020-07-24 23:04:21 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
   
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
+    data='''{
+  "Project": "paksmas",
+  "Site": "mamram",
+  "TimeGenerate": "14:21",
+  "severity": "moti is on fire"
+}'''
     return "Hello"

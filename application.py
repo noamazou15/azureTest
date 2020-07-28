6fd1978897c7e74c19abfee991314acceabf4b36
noamazou15 <61350757+noamazou15@users.noreply.github.com>
2020-07-28 18:01:01 +0300

    Update application.py

--- application.py
+++ application.py
@@ -11,6 +11,7 @@ def hello():
   "Project": "paksmas",
   "Site": "mamram",
   "TimeGenerate": "14:21",
-  "severity": "moti is on fire"
+  "severity": "moti is on fire",
+  "ID":"207882622@idf.il"
 }'''
     return data

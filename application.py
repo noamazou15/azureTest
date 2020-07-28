8dc2d23911b79555f9b8f3b67f7c678dc9c70ecb
noamazou15 <61350757+noamazou15@users.noreply.github.com>
2020-07-28 18:19:54 +0300

    Update application.py

--- application.py
+++ application.py
@@ -7,11 +7,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    data='''{
+    data='''[{
   "Project": "paksmas",
   "Site": "mamram",
   "TimeGenerate": "14:21",
   "severity": "moti is on fire",
   "ID":"207882622@idf.il"
-}'''
+},
+ {
+  "Project": "pakdfdfmas",
+  "Site": "mafsafam",
+  "TimeGenerate": "14sf1",
+  "severity": "moti issfsf fire",
+  "ID":"207882sfsf@idf.il"
+}
+ ]'''
     return data
